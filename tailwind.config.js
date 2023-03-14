f43e4abf93c190c4546a28e4b7c17a67f3e4d72a
Pliskin <arieljanickcarvalho@gmail.com>
2023-03-14 09:30:34 -0100

    Initial commit.
    Done setting up project. Installed tailwindcss and extended theme to add project colors.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brownish: {
+          attention: "#A75D5D",
+
+        }
+      }
+    },
+  },
+  plugins: [],
+}
